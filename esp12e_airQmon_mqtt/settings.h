4392bae54dc33a6047c6cb49de3003d048eda01b
Paul <paulmorris32@gmail.com>
2019-03-08 23:32:25 +0000

    .gitignnore updates

--- esp12e_airQmon_mqtt/settings.h
+++
@@ -1,5 +0,0 @@
-// wifi settings
-// RENAME ME TO settings.h after adding your details below
-
-const char* ssid = "YOURWIFI"; //your WiFi Name
-const char* password = "YOURPASSWORD";  //Your Wifi Password
